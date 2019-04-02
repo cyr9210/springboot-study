85c8214a231d9aa5736826baf11e8eab494af1ab
cyr9210 <jipsyer@naver.com>
2019-04-02 16:52:19 +0900

    first commit

---
+++ 11/src/main/java/bong/springboot11/SpringBoot11Application.java
@@ -0,0 +1,25 @@
+package bong.springboot11;
+
+import org.apache.catalina.Context;
+import org.apache.catalina.LifecycleException;
+import org.apache.catalina.startup.Tomcat;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+@SpringBootApplication
+public class SpringBoot11Application {
+
+    public static void main(String[] args){
+        SpringApplication.run(SpringBoot11Application.class, args);
+
+    }
+
+
+
+}
