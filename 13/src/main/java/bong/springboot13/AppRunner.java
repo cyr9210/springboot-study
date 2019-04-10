a2758e478930b22aa362ad5cc93bd436f4047589
cyr9210 <jipsyer@naver.com>
2019-04-10 16:24:46 +0900

    20190410

--- 13/src/main/java/bong/springboot13/AppRunner.java
+++ 13/src/main/java/bong/springboot13/AppRunner.java
@@ -1,5 +1,7 @@
 package bong.springboot13;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
@@ -20,14 +22,21 @@ public class AppRunner implements ApplicationRunner {
     @Autowired
     private String hello;
 
+    private Logger logger = LoggerFactory.getLogger(AppRunner.class);
+
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        System.out.println("===============");
-        System.out.println(bongProperties.getName());
-//        System.out.println(bongProperties.getAge());
-        System.out.println(bongProperties.getFullName());
-//        System.out.println(bongProperties.getSessionTimeOut());
-        System.out.println(hello);
-        System.out.println("===============");
+//        System.out.println("===============");
+//        System.out.println(bongProperties.getName());
+////        System.out.println(bongProperties.getAge());
+//        System.out.println(bongProperties.getFullName());
+////        System.out.println(bongProperties.getSessionTimeOut());
+//        System.out.println(hello);
+//        System.out.println("===============");
+
+        logger.debug("================");
+        logger.debug(bongProperties.getName());
+        logger.debug(bongProperties.getFullName());
+        logger.debug("================");
     }
 }
