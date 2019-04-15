f6a13c8ecdc90682ed93de5734198e50ec0fde4c
cyr9210 <jipsyer@naver.com>
2019-04-15 21:15:28 +0900

    20190415

---
+++ 16-2/src/main/java/bong/springboot162/account/Account.java
@@ -0,0 +1,21 @@
+package bong.springboot162.account;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Data
+@Entity
+public class Account {
+
+    @Id @GeneratedValue
+    private long id;
+
+    private String username;
+
+    private String password;
+
+
+}
---
+++ 16-2/src/test/java/bong/springboot162/account/AccountRepositoryTest.java
@@ -0,0 +1,60 @@
+package bong.springboot162.account;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.SQLException;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class AccountRepositoryTest {
+
+    @Autowired
+    DataSource dataSource;
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    AccountRepository accountRepository;
+
+    @Test
+    public void di() throws SQLException {
+        try (Connection connection = dataSource.getConnection()) {
+            DatabaseMetaData meta = connection.getMetaData();
+            System.out.println(meta.getURL());
+            System.out.println(meta.getDriverName());
+            System.out.println(meta.getUserName());
+        }
+    }
+
+    @Test
+    public void test(){
+        Account account = new Account();
+        account.setUsername("bong");
+        account.setPassword("pass");
+
+        Account newAccount = accountRepository.save(account);
+
+        assertThat(newAccount).isNotNull();
+
+        Optional<Account> existingAccount = accountRepository.findByUsername(newAccount.getUsername());
+        assertThat(existingAccount).isNotEmpty();
+
+        Optional<Account> nonexistingAccount = accountRepository.findByUsername("kessun");
+        assertThat(nonexistingAccount).isEmpty();
+
+    }
+
+
+}
