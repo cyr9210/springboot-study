8a96a8bfa48de778ae152357252052774fc3c2f1
cyr9210 <jipsyer@naver.com>
2019-09-04 14:51:01 +0900

    Async CompleteFuture사용

--- Async/src/main/java/me/bong/asyncspringboot/SampleService.java
+++ Async/src/main/java/me/bong/asyncspringboot/SampleService.java
@@ -5,16 +5,19 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Executor;
+
 @Service
 @Transactional
 @RequiredArgsConstructor
 @Slf4j
 public class SampleService {
 
-    private final AsyncService asyncService;
+    private final Executor myPool;
 
     public void asyncCall(int i) {
-        asyncService.asyncRun(() -> async(i));
+        CompletableFuture.runAsync(() -> async(i), myPool);
     }
 
     public void async(int i) {
